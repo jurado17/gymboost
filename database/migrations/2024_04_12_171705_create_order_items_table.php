49f71340623be2ec68c7e4be6ff99e79072642bc
Luis Jurado <luisjurador9@gmail.com>
2024-06-12 10:03:49 +0200

    second commit

--- database/migrations/2024_04_12_171705_create_order_items_table.php
+++ database/migrations/2024_04_12_171705_create_order_items_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('order_id')->references('id')->on('stock');
+            $table->foreignId('order_id')->references('id')->on('orders');
             $table->foreignId('product_id')->references('id')->on('stock_products');
             $table->integer('quantity');
             $table->decimal('unit_price');
