43d5c7412f5623533598c4463c432ff7e60d21f8
Luis Jurado <luisjurador9@gmail.com>
2024-06-12 17:07:38 +0200

    commit fourteen

--- database/seeders/UserAddressesTableSeeder.php
+++ database/seeders/UserAddressesTableSeeder.php
@@ -11,8 +11,17 @@ class UserAddressesTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
+        
+        // Obtener todos los ids de usuarios existentes
+        $userIds = DB::table('users')->pluck('id');
 
-        foreach(range(1, 10) as $index) {
+        // Verifica que haya usuarios en la tabla 'users'
+        if ($userIds->isEmpty()) {
+            $this->command->info('No users found, skipping user_addresses seeding.');
+            return;
+        }
+
+        foreach ($userIds as $userId) {
             DB::table('user_addresses')->insert([
                 'type' => $faker->word,
                 'address1' => $faker->address,
@@ -21,7 +30,7 @@ class UserAddressesTableSeeder extends Seeder
                 'zip_code' => $faker->postcode,
                 'isMain' => $faker->boolean,
                 'country_code' => $faker->countryCode,
-                'user_id' => $faker->numberBetween(1, 10),
+                'user_id' => $userId,
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
