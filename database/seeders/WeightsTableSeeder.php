3f72d914cdbd5b72350589fbd087cbf60b061cfe
Luis Jurado <luisjurador9@gmail.com>
2024-06-13 18:38:36 +0200

    commit thirty-nine

--- database/seeders/WeightsTableSeeder.php
+++ database/seeders/WeightsTableSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
@@ -11,22 +10,15 @@ class WeightsTableSeeder extends Seeder
 {
     public function run()
     {
-        $specificWeights = [
-            ['name' => 'Monodosis(50g)', 'weight_g' => 50, 'price' => 0.05],
-            ['name' => '200g', 'weight_g' => 200, 'price' => 0.20],
-            ['name' => '500g', 'weight_g' => 500, 'price' => 0.50],
-            ['name' => '1Kg', 'weight_g' => 1000, 'price' => 1.00],
-            ['name' => '2Kg', 'weight_g' => 2000, 'price' => 2.00],
-            ['name' => '4Kg', 'weight_g' => 4000, 'price' => 4.00],
-        ];
+        $faker = Faker::create();
 
-        foreach ($specificWeights as $weight) {
+        foreach(range(1, 5) as $index) {
             DB::table('weights')->insert([
-                'name' => $weight['name'],
-                'weight_g' => $weight['weight_g'],
-                'price' => $weight['price'],
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
+                'name' => $faker->word,
+                'weight_g' => $faker->numberBetween(500, 5000),
+                'price' => $faker->randomFloat(2, 0.5, 100),
+                'created_at' => now(),
+                'updated_at' => now(),
             ]);
         }
     }
